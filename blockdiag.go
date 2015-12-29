14f45846bb3f6d2f3dd142a16139a7cc2c0f2747
Lucas Bremgartner <lucas@bremis.ch>
2015-12-29 09:34:18 +0100

    Added first version of circular detection

--- blockdiag.go
+++ blockdiag.go
@@ -1,9 +1,53 @@
 package blockdiag
 
+import (
+	"fmt"
+
+	linq "github.com/ahmetalpbalkan/go-linq"
+)
+
 type Diag struct {
 	Name  string
 	Nodes map[string]*Node
 	Edges map[string]*Edge
+}
+
+func (diag *Diag) FindCircular() bool {
+	for _, n := range diag.Nodes {
+		visitedNodes := &nodes{}
+
+		fmt.Println("\nStart from Node:", n.Name)
+		if !visitedNodes.exists(n.Name) {
+			visitedNodes.keys = append(visitedNodes.keys, n.Name)
+		}
+		for _, c := range n.getChildNodes() {
+			fmt.Println("Child:", c.Name)
+			subFindCircular(c, visitedNodes)
+		}
+	}
+
+	// Wrong
+	return false
+}
+
+func subFindCircular(n *Node, visitedNodes *nodes) {
+	fmt.Println("In Node:", n.Name)
+	if visitedNodes.exists(n.Name) {
+		fmt.Println("Found already visited Node:", n.Name)
+		fmt.Print("Path: ")
+		for _, p := range visitedNodes.keys {
+			fmt.Print(p, " -> ")
+		}
+		fmt.Println(n.Name)
+		return
+	}
+	visitedNodes.keys = append(visitedNodes.keys, n.Name)
+
+	for _, c := range n.getChildNodes() {
+		fmt.Println("Child:", c.Name)
+		subFindCircular(c, visitedNodes)
+	}
+	visitedNodes.keys = visitedNodes.keys[:len(visitedNodes.keys)-1]
 }
 
 type Node struct {
@@ -13,8 +57,28 @@ type Node struct {
 	Edges []*Edge
 }
 
+func (n *Node) getChildNodes() (children []*Node) {
+	for _, e := range n.Edges {
+		if e.Start == n {
+			children = append(children, e.End)
+		}
+	}
+	return
+}
+
 type Edge struct {
 	Start *Node
 	End   *Node
 	Name  string
+}
+
+type nodes struct {
+	keys []string
+}
+
+func (n *nodes) exists(key string) bool {
+	ret, _ := linq.From(n.keys).AnyWith(func(s linq.T) (bool, error) {
+		return s.(string) == key, nil
+	})
+	return ret
 }
