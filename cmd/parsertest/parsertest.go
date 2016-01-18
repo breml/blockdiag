cdc732cb670328c39122c75e34d518d50edf0fd0
Lucas Bremgartner <lucas@bremis.ch>
2016-01-18 20:57:27 +0100

    Test command

---
+++ cmd/parsertest/parsertest.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"strings"
+
+	"github.com/breml/blockdiag"
+)
+
+func main() {
+	simple := ""
+	simple =
+		`blockdiag {
+   A -> B -> C;
+   A -> D;
+}`
+
+	simple =
+		`blockdiag {
+   A -> B -> C;
+   A -> D;
+}`
+
+	simple =
+		`blockdiag {
+			node_width = 128;
+			A -> B -> C -> D;
+			A -> E -> F -> G;
+		}`
+
+	got, err := blockdiag.ParseReader("simple.diag", strings.NewReader(simple))
+	if err != nil {
+		log.Fatal("Parse error:", err)
+	}
+	diag := got.(blockdiag.Diag)
+
+	diag.PlaceInGrid()
+	fmt.Printf("%s\n", diag.GridString())
+
+	// fmt.Println("=", diag)
+
+	/*
+		for _, e := range diag.Edges {
+			fmt.Println(e.Name)
+		}
+	*/
+
+	//fmt.Println("Circular: ", diag.FindCircular())
+
+	//fmt.Printf("Diag: %#v\n", &diag)
+}
