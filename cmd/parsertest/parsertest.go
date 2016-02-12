42685a4947b05f2133073ad30870a648002aa74c
Lucas Bremgartner <lucas@bremis.ch>
2016-02-12 22:51:23 +0100

    Cleanup parsertest

--- cmd/parsertest/parsertest.go
+++ cmd/parsertest/parsertest.go
@@ -12,33 +12,12 @@ func main() {
 	simple := ""
 	simple =
 		`blockdiag {
-   A -> B -> C;
-   A -> D;
+	A -> B;
+	H -> I;
+	E -> F -> B;
+	H -> J;
+	J -> K;
 }`
-
-	simple =
-		`blockdiag {
-   A -> B -> C;
-   A -> D;
-}`
-
-	simple =
-		`blockdiag {
-			node_width = 128;
-			A -> B -> C -> D;
-			B -> F -> G -> X -> Y;
-			C -> E;
-			H -> I -> J -> H;
-		}`
-
-	simple =
-		`blockdiag {
-			A -> B -> C;
-			B -> D -> E -> H;
-			A -> F -> E;
-			F -> G;
-			X -> Y;
-		}`
 
 	got, err := blockdiag.ParseReader("simple.diag", strings.NewReader(simple))
 	if err != nil {
@@ -48,16 +27,4 @@ func main() {
 
 	diag.PlaceInGrid()
 	fmt.Printf("%s\n", diag.String())
-
-	// fmt.Println("=", diag)
-
-	/*
-		for _, e := range diag.Edges {
-			fmt.Println(e.Name)
-		}
-	*/
-
-	//fmt.Println("Circular: ", diag.FindCircular())
-
-	//fmt.Printf("Diag: %#v\n", &diag)
 }
